cbd82dded30b522fd765cfc2a5b23c062dd8ca61
DonaLd-D <1376825153@qq.com>
2020-11-26 17:40:05 +0800

    window.opener.postMessage

--- main.js
+++ main.js
@@ -12,13 +12,13 @@ function createWindow () {
   })
 
   win.webContents.openDevTools()
-  win.loadFile('index2.html')
+  win.loadFile('index3.html')
   require('./main/main.js')
 
-  var view = new BrowserView()
-  win.setBrowserView(view)
-  view.setBounds({x:0,y:100,width:600, height:400})
-  view.webContents.loadURL('https://jspang.com')
+//   var view = new BrowserView()
+//   win.setBrowserView(view)
+//   view.setBounds({x:0,y:100,width:600, height:400})
+//   view.webContents.loadURL('https://jspang.com')
 }
 
 app.whenReady().then(createWindow)
